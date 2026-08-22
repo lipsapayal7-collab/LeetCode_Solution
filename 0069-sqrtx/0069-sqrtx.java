de4d89f7451c2c58568867471e26970cd5968642
Lipsarani Sahoo <lipsapayal7@gmail.com>
2026-08-22 21:08:31 +0530

    Time: 1 ms (98.35%), Space: 42.4 MB (90.56%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2){
+            return x;
+        }
+        int l=1,r=x/2,a=0;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(m<=x/m){
+                a=m;
+                l=m+1;
+            }
+            else r=m-1;
+        }
+        return a;
+    }
+}
